9f8d8b60da05bd2d26edce9b1c1cc6e78709f25d
Alexis <lecuyalex@gmail.com>
2021-01-26 14:47:25 -0500

    Start footer

--- AppBundle/js/app.js
+++ AppBundle/js/app.js
@@ -5,3 +5,22 @@ menu.addEventListener('click',function (){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+window.onscroll = function () {
+    myFunction()
+};
+
+// Get the navbar
+var navbar = document.getElementById("nav-container");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
