08dca9e9d17564757d6d2c58f21fbeb945ef19f7
Alexis <lecuyalex@gmail.com>
2021-02-19 10:00:08 -0500

    add event in admin

--- AppBundle/Ressources/php/bd.php
+++ AppBundle/Ressources/php/bd.php
@@ -1,12 +1,12 @@
 <?php
 try {
-    $connexion = new PDO('mysql:host=localhost;dbname=sessionweb2021;port=3308', 'root', '');
+    $connexion = new PDO('mysql:host=206.167.140.56;dbname=h2021_420626ri_gr01_équipe_6;port=3306', '1846551', '1846551');
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
     echo json_encode("erreur" . $e->getMessage());
 }
-if (isset($_POST["requete"])){
+if (isset($_POST["requete"])) {
     if ($_POST["requete"] == "selectEvents") {
         try {
             $stmt = $connexion->prepare("SELECT * FROM EVENTS");
@@ -26,8 +26,23 @@ if (isset($_POST["requete"])){
         } catch (PDOException $e) {
             echo json_encode($e);
         }
+    } elseif ($_POST["requete"] == "insertEvent") {
+        echo 'tsts';
+        try {
+            $stmt = $connexion->prepare("INSERT INTO EVENTS ( name, description, location, dateStart, dateEnd, maxCapacity) VALUES (:name, :description, :location, :dateStart, :dateEnd, :maxCapacity)");
+            $stmt->bindParam(':name', $_POST["name"]);
+            $stmt->bindParam(':description', $_POST["description"]);
+            $stmt->bindParam(':location', $_POST["location"]);
+            $stmt->bindParam(':dateStart', $_POST["dateStart"]);
+            $stmt->bindParam(':dateEnd', $_POST["dateEnd"]);
+            $stmt->bindParam(':maxCapacity', $_POST["maxCapacity"]);
+            $stmt->execute();
+            echo json_encode(true);
+        } catch (PDOException $e) {
+            echo json_encode($e);
+        }
     }
-}else
+}
 
 
 ?>
