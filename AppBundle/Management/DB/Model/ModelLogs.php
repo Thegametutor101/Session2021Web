28c540c36d1c24d30891ab349aa7d8ca975b995e
Antoine <darkometal277@gmail.com>
2021-03-04 00:35:12 -0500

    Changements utilisateurs et BD

--- AppBundle/Management/DB/Model/ModelLogs.php
+++ AppBundle/Management/DB/Model/ModelLogs.php
@@ -13,21 +13,4 @@ class ModelLogs
         $class = new Connection();
         $this->connection = $class->getConnection();
     }
-
-    /**
-     * @return list
-     */
-    public function selectAllCroissant(): bool
-    {
-        try {
-            $stmt = $this->connection->prepare(
-                "SELECT * FROM logs");
-            $stmt->execute();
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            echo json_encode($results);
-        } catch (PDOException $e) {
-            echo json_encode($e);
-        }
-    }
 }
-?>
